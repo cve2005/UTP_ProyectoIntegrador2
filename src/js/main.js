2b9b8eafdd4f6a136e3275fc910f09b32490834a
jyarango <jyarango.13@gmail.com>
2023-10-30 14:13:25 -0500

    20-rutas

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,9 @@
 const userInfo =  JSON.parse(sessionStorage.getItem('userInfo'));
 
 const nombreSesion = document.getElementById('nombreSesion');
-nombreSesion.textContent = userInfo.cna_user.usu_nombre;
+nombreSesion.textContent = userInfo.cna_user.usu_nombre
+
+const tipoRol = document.getElementById('estadoSesion');
+tipoRol.textContent = userInfo.cna_user.usu_rol ;
+
 
