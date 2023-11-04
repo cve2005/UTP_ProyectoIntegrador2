e970c67d2cc5af27eba53055ec326d6e9c9cc29d
jyarango <jyarango.13@gmail.com>
2023-11-03 23:02:02 -0500

    26-fixclienteall

--- src/js/empresacliente.js
+++ src/js/empresacliente.js
@@ -26,11 +26,6 @@ agregarButton.addEventListener('click', () => {
 
   conexApi.post(`empresa`, data).then((res) => {
     console.log('Se agrego correctamente los datos')
-    console.log(res.data.data)
-    console.log(res.data)
-    console.log(res)
-
-
   })
     .catch((error) => {
       console.error('Hubo un error:', error);
@@ -59,7 +54,7 @@ buscarButton.addEventListener('click', () => {
 });
 
 
-//Agregar usuario
+//Agregar cliente
 const agregarUsuarioButton = document.getElementById('btnRegistrarUsuario')
 agregarUsuarioButton.addEventListener('click', () => {
   const fusu_nombre = document.getElementById('fusu_nombre').value;
@@ -67,11 +62,15 @@ agregarUsuarioButton.addEventListener('click', () => {
   const fusu_apellido = document.getElementById('fusu_apellido').value;
   const fusu_dni = document.getElementById('fusu_dni').value;
   const fusu_email = document.getElementById('fusu_email').value;
-  //const fusu_contrasena = document.getElementById('fusu_dni').value;
+
+  //contraseña el dni
+  const fusu_contrasena = document.getElementById('fusu_dni').value;
   //const fusu_contrasena = document.getElementById('fusu_contrasena').value;
   const fusu_telefono = document.getElementById('fusu_telefono').value;
   const fusu_direccion = document.getElementById('fusu_direccion').value;
-  //const fusu_rol =5;
+  const femp_id =document.getElementById('fidEmpresa').value
+
+  const fusu_rol =5;
 
   //const fusu_comision =document.getElementById('fusu_comision').value;
   //const fusu_comision = 0;
@@ -82,15 +81,17 @@ agregarUsuarioButton.addEventListener('click', () => {
     usu_apellido: fusu_apellido,
     usu_dni: fusu_dni,
     usu_email: fusu_email,
-    //usu_contrasena: fusu_contrasena,
+    usu_contrasena: fusu_contrasena,
     usu_telefono: fusu_telefono,
     usu_direccion: fusu_direccion,
-    //usu_rol: fusu_rol,
+    usu_rol: fusu_rol,
+    emp_id:femp_id
     //usu_comision: fusu_comision
   }
   console.log(data)
 
-  conexApi.patch(`usuario/${id}`, data).then((res) => {
+  conexApi.post(`usuario`, data).then((res) => {
+    console.log(res)
     console.log('Se agrego correctamente los datos')
   })
     .catch((error) => {
@@ -107,7 +108,7 @@ async function cargarEditarUsuarios() {
     document.getElementById('fruc').value = usuario.emp_id.emp_ruc
     document.getElementById('fusu_telefono').value = usuario.emp_id.emp_telefono
     document.getElementById('fusu_direccion').value = usuario.emp_id.emp_direccion
-    document.getElementById('fidEmpresa').value = usuario.emp_id.emp_id.emp_id
+    document.getElementById('fidEmpresa').value = usuario.emp_id.emp_id
    // document.getElementById('fidCliente').value = usuario.usu_id
     document.getElementById('fusu_dni').value = usuario.usu_dni
     document.getElementById('fusu_nombre').value = usuario.usu_nombre
@@ -124,6 +125,50 @@ async function cargarEditarUsuarios() {
       console.error('Hubo un error:', error);
     });
 }
+
+
+//Editar cliente
+const editarClienteButton = document.getElementById('btnActualizarUsuario')
+editarClienteButton.addEventListener('click', () => {
+  const fusu_nombre = document.getElementById('fusu_nombre').value;
+  //const fid_empresa = document.getElementById('fidEmpresa').value;
+  const fusu_apellido = document.getElementById('fusu_apellido').value;
+  const fusu_dni = document.getElementById('fusu_dni').value;
+  const fusu_email = document.getElementById('fusu_email').value;
+  const femp_id =document.getElementById('fidEmpresa').value
+  const fusu_telefono = document.getElementById('fusu_telefono').value;
+
+  //contraseña el dni
+  // const fusu_contrasena = document.getElementById('fusu_dni').value;
+  // //const fusu_contrasena = document.getElementById('fusu_contrasena').value;
+  // const fusu_direccion = document.getElementById('fusu_direccion').value;
+  // const fusu_rol =5;
+  // const fusu_comision =document.getElementById('fusu_comision').value;
+  // //const fusu_comision = 0;
+
+
+  const data = {
+    usu_nombre: fusu_nombre,
+    usu_apellido: fusu_apellido,
+    usu_dni: fusu_dni,
+    usu_email: fusu_email,
+    // usu_contrasena: fusu_contrasena,
+    usu_telefono: fusu_telefono,
+    // usu_direccion: fusu_direccion,
+    // usu_rol: fusu_rol,
+    emp_id:femp_id
+    //usu_comision: fusu_comision
+  }
+  console.log(data)
+
+  conexApi.patch(`usuario/${id}`, data).then((res) => {
+    console.log(res)
+    console.log('Se agrego correctamente los datos')
+  })
+    .catch((error) => {
+      console.error('Hubo un error:', error);
+    });
+});
 
 window.addEventListener('load', function () {
   if (window.location.href.includes('editar-usuario.html')) {
